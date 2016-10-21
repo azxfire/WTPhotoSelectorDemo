2b9a9003b6019564f56196f69a006d8be6d46ccc
wangtao <wangtaosecret@gmail.com>
2016-10-21 14:13:52 +0800

    Initial Commit

---
+++ WTPhotoSelectorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WTPhotoSelectorDemo
+//
+//  Created by taowang on 2016/10/21.
+//  Copyright © 2016年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WTPhotoSelectorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTPhotoSelectorDemo
+//
+//  Created by taowang on 2016/10/21.
+//  Copyright © 2016年 mm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
